ae1ef7943feb370839bf7d02326eef44ec187892
Jack Kim <hion8800@gmail.com>
2021-08-20 17:50:54 +0900

    3rd setting

--- students/dongjunKim/westagram/settings.py
+++ students/dongjunKim/westagram/settings.py
@@ -12,19 +12,15 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from my_settings import DATABASES, SECRET_KEY 
-import pymysql
-
-pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7e6vtv7dwv--gq2wqzhjg-u(k)r8e@k5kvv6*$qp*!d+2mr=3#'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
