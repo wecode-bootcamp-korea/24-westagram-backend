52a02d1db9e5b008ff55da0b80ecb4dd71f2ca45
Young Soo Koh <kohys92@gmail.com>
2021-08-20 19:30:49 +0900

    Add: modified models.py

--- students/youngsoo/users/models.py
+++ students/youngsoo/users/models.py
@@ -3,5 +3,11 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
     name = models.CharField(max_length=45)
-    email = models.CharField(max_length=100)
+    email = models.CharField(max_length=45)
     password = models.CharField(max_length=45)
+    phone_num = models.IntegerField()
+    other_info = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = 'users'
+
