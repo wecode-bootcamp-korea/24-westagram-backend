3e5e2cde9489444b166e872e11ec84671a519774
YeJun <hayejun1013@naver.com>
2021-08-21 20:07:25 +0900

    Add user application files and create user class on models

---
+++ students/yejun/users/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class User(models.Model):
+    user_name     = models.CharField(max_length=64)
+    email         = models.EmailField()
+    password      = models.CharField(max_length=128)
+    phone_number  = models.CharField(max_length=32)
+    date_of_birth = models.DateField()
+
+    class Meta:
+        db_table = "users"
