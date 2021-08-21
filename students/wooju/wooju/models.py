27d990818cd3738e0a853d43c310cff49b161bc3
신우주 <newspace@sin-ujuui-MacBookPro.local>
2021-08-21 14:37:22 +0900

    project modeling

---
+++ students/wooju/wooju/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class User(models.Model):
+    name            = models.CharField(max_length=45)
+    emails          = models.CharField(max_length=300)
+    password        = models.CharField(max_length=45)
+    phone_number    = models.CharField(max_length=20)
+    date_of_birth   = models.DateField()
+
+    class Meta:
+        db_table    = 'users'
