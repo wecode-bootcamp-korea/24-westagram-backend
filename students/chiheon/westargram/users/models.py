44394890d55efd544b52ba6ebdb006123b0c78ad
Chi heon Song <hrpp1353@gmail.com>
2021-08-21 14:28:57 +0900

    Add : users app & models.py, migrate my modeling

--- students/chiheon/westargram/users/models.py
+++ students/chiheon/westargram/users/models.py
@@ -1,5 +1,5 @@
 from django.db   import models
-from phone_fiedl import PhoneField
+from phone_field import PhoneField
 
 # Create your models here.
 
--- students/chiheon/westargram/westargram/settings.py
+++ students/chiheon/westargram/westargram/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders'
-    'phone_field'
-    'users'
+    'corsheaders',
+    'phone_field',
+    'users',
 ]
 
 MIDDLEWARE = [
