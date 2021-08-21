bcd713874feef471cb764675a48b4cd21f3ab0f1
Young Soo Koh <kohys92@gmail.com>
2021-08-21 15:41:52 +0900

    Modified: Users class in models.py, added more columns of user info

--- students/youngsoo/users/models.py
+++ students/youngsoo/users/models.py
@@ -2,11 +2,13 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    name = models.CharField(max_length=45)
-    email = models.CharField(max_length=45)
-    password = models.CharField(max_length=45)
-    phone_num = models.IntegerField()
-    other_info = models.CharField(max_length=100)
+    name            = models.CharField(max_length = 45)
+    email           = models.EmailField(max_length = 250)
+    password        = models.CharField(max_length = 45)
+    phone_num       = models.CharField(max_length = 20)
+    date_of_birth   = models.DateField()
+    gender          = models.CharField(max_length = 20)
+    address         = models.CharField(max_length = 2000)
 
     class Meta:
         db_table = 'users'
