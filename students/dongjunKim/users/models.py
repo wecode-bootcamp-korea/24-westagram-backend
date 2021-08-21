708205c19ba9b646b361036d81ccd2282934a22b
Jack Kim <hion8800@gmail.com>
2021-08-21 11:47:57 +0900

    ADD : models.py, UPDATE : settings.py

--- students/dongjunKim/users/models.py
+++ students/dongjunKim/users/models.py
@@ -1,14 +1,9 @@
 from django.db import models
 
 class User(models.Model):
-    GENDER = (
-        ('M', 'MAN'),
-        ('W', 'WOMAN')
-    )
     name = models.CharField(max_length=30)
-    email = models.CharField(max_length=100)
+    email = models.CharField(max_length=60)
     password = models.CharField(max_length=25)
     phone = models.CharField(max_length=15)
-
     class Meta:
         db_table = 'users'
