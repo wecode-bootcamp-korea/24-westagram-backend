ac10a7c4b02030cfb8bba0f2379b9a4084907f55
Jinatra <jinatra@Jinatraui-MacBookAir.local>
2021-08-21 12:37:46 +0900

    models.py revision

--- students/woojin/users/models.py
+++ students/woojin/users/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class User(models.Model):
-    name        = models.CharField(max_length=45)
-    email       = models.EmailField()
-    password    = models.CharField(max_length=200)
-    contact     = models.CharField(max_length=20)
+    name              = models.CharField(max_length=45)
+    email             = models.EmailField()
+    password          = models.CharField(max_length=200)
+    cell_phone_number = models.CharField(max_length=20)
     
     class Meta:
         db_table = 'users'
