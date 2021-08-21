03880b61e1a40b231a80490d25cdc94fbf3f6fea
Chi heon Song <hrpp1353@gmail.com>
2021-08-21 14:19:38 +0900

    Add : users app & models.py

---
+++ students/chiheon/westargram/users/models.py
@@ -0,0 +1,36 @@
+from django.db   import models
+from phone_fiedl import PhoneField
+
+# Create your models here.
+
+class User(models.Model):
+    first_name    = models.CharField(
+            max_length = 10,  
+            blank      = False
+            )
+    last_name     = models.CharField(
+            max_length = 10,  
+            blank      = False
+            )
+    email         = models.EmailField(
+            max_length = 50, 
+            blank      = False
+            )
+    password      = models.CharField(
+            max_length = 100, 
+            blank      = False
+            )
+    phone_number  = PhoneField(
+            blank      = False
+            )
+    gender_choice =(
+            ('M', 'Man'),
+            ('W', 'Woman')
+            )
+    gender        = models.CharField(
+            max_length = 1,
+            choices    = gender_choice
+            )
+    birth         = models.DateField(
+            )
+    
--- students/chiheon/westargram/westargram/settings.py
+++ students/chiheon/westargram/westargram/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders'
+    'phone_field'
+    'users'
 ]
 
 MIDDLEWARE = [
