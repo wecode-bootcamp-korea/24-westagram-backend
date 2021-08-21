da6014547996f10b6cb2dd49b806ccd564cc3c0d
fall031 <fall900802@gmail.com>
2021-08-21 22:12:58 +0900

    project models.py

--- students/hyeonmuk/hyeonmuk/models.py
+++ students/hyeonmuk/hyeonmuk/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 
 class User(models.Model):
-    name = models.CharField(max_length=40)
-    email = models.CharField(max_length=40)
-    password = models.CharField(max_length=20)
+    name           = models.CharField(max_length=40)
+    email          = models.CharField(max_length=40)
+    password       = models.CharField(max_length=20)
     contact_mobile = models.CharField(max_length=20)
-    nickname = models.CharField(max_length=40)
-    address = models.CharField(max_length=300)
+    nickname       = models.CharField(max_length=40)
+    address        = models.CharField(max_length=300)
 
     class Meta:
-        db_table = 'users'
+        db_table   = 'users'
