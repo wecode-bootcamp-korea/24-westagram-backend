7fbe6e10d2db13423e4545d898d14b17fdcacbf7
Chi heon Song <hrpp1353@gmail.com>
2021-08-22 14:44:24 +0900

    Modified: models.py

--- students/chiheon/westargram/users/models.py
+++ students/chiheon/westargram/users/models.py
@@ -4,33 +4,15 @@ from phone_field import PhoneField
 # Create your models here.
 
 class User(models.Model):
-    first_name    = models.CharField(
-            max_length = 10,  
-            blank      = False
-            )
-    last_name     = models.CharField(
-            max_length = 10,  
-            blank      = False
-            )
-    email         = models.EmailField(
-            max_length = 50, 
-            blank      = False
-            )
-    password      = models.CharField(
-            max_length = 100, 
-            blank      = False
-            )
-    phone_number  = PhoneField(
-            blank      = False
-            )
+    first_name    = models.CharField(max_length = 10)
+    last_name     = models.CharField(max_length = 10)
+    email         = models.EmailField(max_length = 50)
+    password      = models.CharField(max_length = 100)
+    phone_number  = PhoneField()
     gender_choice =(
             ('M', 'Man'),
             ('W', 'Woman')
             )
-    gender        = models.CharField(
-            max_length = 1,
-            choices    = gender_choice
-            )
-    birth         = models.DateField(
-            )
+    gender        = models.CharField(max_length = 1,choices    = gender_choice)
+    birth         = models.DateField()
     
--- students/chiheon/westargram/users/views.py
+++ students/chiheon/westargram/users/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+import json
+from django.http  import JsonResponse
+from django.views import View
+from users.models import User
+
+class UserView(View):
+    def post(self,request):
+        try:
+            data       = json.loads(request.body)
+            user = User.objects.create(
+                    first_name = data['first_name'],
+                    last_name  = data['last_name'],
+                    email      = data['email'],
+                    password   = data['password'],
+                    phone_number = data['phone_number'],
+                    gender     = data['gender']
+                    birth      = data['birthday']
+                    )
+            return JsonResponse(
+                    {'message' : 'CREATED'}, 
+                    status = 201
+                    )
+        except KeyError:
+            return JsonResponse(
+                    {'message' : 'KEY_ERROR'}, 
+                    status = 400
+                    )
+
