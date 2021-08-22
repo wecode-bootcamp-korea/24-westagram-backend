f0672b6613750743f4832bf6d33c26ff53ff169e
Chi heon Song <hrpp1353@gmail.com>
2021-08-22 19:22:19 +0900

    Add : views.py, validation.py, urls.py

--- students/chiheon/westargram/users/validation.py
+++ students/chiheon/westargram/users/validation.py
@@ -1,1 +1,18 @@
+import re
 
+def Validate_email(email):
+    email_regex = re.compile('[^a-zA-Z0-9_]')
+    email_id = email.split('@')[0]
+    if email_regex.search(email_id):
+        return 'ValidationError'
+    elif len(email_id)==0:
+        return 'KeyError'
+    return 'Success'
+
+def Validate_password(password):
+    password_regex = re.compile('[^a-zA-Z0-9_]')
+    if len(password)==0:
+        return 'KeyError'
+    elif password.isdigit() or len(password) < 8 or not password_regex.search(password):
+        return 'ValidationError'
+    return 'Success'
--- students/chiheon/westargram/users/views.py
+++ students/chiheon/westargram/users/views.py
@@ -6,27 +6,40 @@ import json
 from django.http  import JsonResponse
 from django.views import View
 from users.models import User
+from . validation import *
 
 class UserView(View):
     def post(self,request):
+        data = json.loads(request.body)
+        email_data    = data['email']
+        password_data = data['password']
         try:
-            data       = json.loads(request.body)
+            if not '@' in email_data or Validate_email(email_data)=='ValidationError' or Validate_password(password_data)=='ValidationError':
+                return JsonResponse(
+                        {'message' : 'ValidationError'},
+                        status = 400
+                        )
+            elif Validate_email(email_data)=='KeyError' or Validate_password(password_data)=='KeyError':
+                return JsonResponse(
+                        {'message' : 'KeyError'},
+                        status = 400
+                        )
             user = User.objects.create(
                     first_name = data['first_name'],
                     last_name  = data['last_name'],
                     email      = data['email'],
                     password   = data['password'],
                     phone_number = data['phone_number'],
-                    gender     = data['gender']
-                    birth      = data['birthday']
+                    gender     = data['gender'],
+                    birth      = data['birth']
                     )
             return JsonResponse(
-                    {'message' : 'CREATED'}, 
+                    {'message' : 'SUCCESS'}, 
                     status = 201
                     )
-        except KeyError:
+        except Exception as e:
             return JsonResponse(
-                    {'message' : 'KEY_ERROR'}, 
+                    {'message' : f'{e}'},
                     status = 400
                     )
 
