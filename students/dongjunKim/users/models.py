59b7a6f9aefe57bc2258a66a1cac59d1b6f9b9d8
Jack Kim <hion8800@gmail.com>
2021-08-23 02:10:11 +0900

    UPDATE : models.py User model field max_length

--- students/dongjunKim/users/models.py
+++ students/dongjunKim/users/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 class User(models.Model):
     name     = models.CharField(max_length=30)
-    email    = models.CharField(max_length=100)
-    password = models.CharField(max_length=25)
-    phone    = models.CharField(max_length=15)
+    email    = models.CharField(max_length=120)
+    password = models.CharField(max_length=120)
+    phone    = models.CharField(max_length=25)
     
     class Meta:
         db_table = 'users'
