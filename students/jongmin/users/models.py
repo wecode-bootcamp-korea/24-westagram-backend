0edfa50b7923034bcaba7340902186fbfdadc688
Gouache-studio <zoobellmint@naver.com>
2021-08-23 00:33:15 +0900

    [주종민] Misson 2: 모델링

--- students/jongmin/users/models.py
+++ students/jongmin/users/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    name = models.CharField(max_length=45)
-    email = models.CharField(max_length=100)
+    name     = models.CharField(max_length=45)
+    email    = models.CharField(max_length=100)
     phone_id = models.IntegerField()
     etc_info = models.CharField(max_length=200)
+    
