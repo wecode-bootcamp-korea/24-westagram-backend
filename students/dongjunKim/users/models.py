88e582e3088ea83c1ae175e0ef73259ea6de0256
Jack Kim <82230389+JackAwesomeKim@users.noreply.github.com>
2021-08-23 01:35:32 +0900

    Update students/dongjunKim/users/models.py
    기존 주석 삭제
    
    Co-authored-by: redsaumon <51191484+redsaumon@users.noreply.github.com>

--- students/dongjunKim/users/models.py
+++ students/dongjunKim/users/models.py
@@ -5,12 +5,6 @@ class User(models.Model):
     email    = models.CharField(max_length=100)
     password = models.CharField(max_length=25)
     phone    = models.CharField(max_length=15)
+    
     class Meta:
         db_table = 'users'
-
-    #다음과 같은 필드들이 추가될 수 있음.
-    #주민번호, 성별, 주소, ..
-    
-    #향후 프론트 파트와의 협업을 진행할 때 달라질 수 있음.
-    #현재 진행하고 있는 프로젝트는 '위스타그램 - 벡앤드' 이므로
-    #기본 필드만 추가하였음.
