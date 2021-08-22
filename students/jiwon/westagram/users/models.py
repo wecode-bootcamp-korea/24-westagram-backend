6e8b030f2cc337eca17f12486cb9e0bc17a92f2b
Jiwon <jiwon@Jiwonui-MacBookAir.local>
2021-08-22 13:14:54 +0900

    project models_2

--- students/jiwon/westagram/users/models.py
+++ students/jiwon/westagram/users/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 class User(models.Model):
-    name = models.CharField(max_length=45)
-    email = models.CharField(max_length=300)
-    password = models.CharField(max_length=45)
-    cellphone = models.CharField(max_length=45)
-    favorite_food = models.CharField(max_length=80)
+    name            = models.CharField(max_length=45)
+    email           = models.CharField(max_length=300)
+    password        = models.CharField(max_length=45)
+    phone_number    = models.CharField(max_length=45)
+    favorite_food   = models.CharField(max_length=80)
     
     class Meta:
-        db_table = 'users' #데이터베이스 table 네임 지정
+        db_table = 'users'
