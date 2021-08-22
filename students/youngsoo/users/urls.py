e6ef85c6b1f919d53a003c3a74b72f2a04f92761
Young Soo Koh <kohys92@gmail.com>
2021-08-22 22:01:20 +0900

    Modify: UserView --> SignupView, changed the url path

--- students/youngsoo/users/urls.py
+++ students/youngsoo/users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import UserView
+from .views import SignupView, UserView
 
 urlpatterns=[
-    path('register', UserView.as_view())
+    path('signup', SignupView.as_view()),
 ]
