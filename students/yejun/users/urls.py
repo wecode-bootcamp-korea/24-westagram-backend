2d28a7b3a721e1408acf8bc5782950ce32af9f4d
YeJun <hayejun1013@naver.com>
2021-08-23 17:39:22 +0900

    Add: Create post method on view file about sign up users

---
+++ students/yejun/users/urls.py
@@ -0,0 +1,4 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [path("/sign-in", views.SignInView.as_view())]
--- students/yejun/users/views.py
+++ students/yejun/users/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+import json
+import re
 
-# Create your views here.
+from django.http import JsonResponse
+from django.views import View
+
+from .models import User
+
+
+class SignInView(View):
+    # 이메일 validation 과정에서 오류가 나면 예외처리 하여야 한다
+    # 패스워드 validation 과정에서 오류가 나면 예외처리 하여야 한다
+
+    def post(self, request):
+        data = json.loads(request.body)
+
+        try:
+            if User.objects.filter(email=data["email"]).exists():
+                return JsonResponse({"message": "EMAIL Already Exist"}, status=400)
+            else:
+                email_validator = re.compile(
+                    "^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+                )
+                password_validator = re.compile(
+                    "^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$"
+                )
+                is_email_valid = email_validator.match(data["email"])
+                is_password_valid = password_validator.match(data["password"])
+
+                if not (is_email_valid and is_password_valid):
+                    return JsonResponse({"message": "INVALID INPUT FORMAT"}, status=400)
+
+            User.objects.create(
+                name=data["name"],
+                email=data["email"],
+                password=data["password"],
+                phone_number=data["phone_number"],
+                date_of_birth=data["date_of_birth"],
+            )
+
+            return JsonResponse({"message": "SUCCESS"}, status=201)
+
+        except KeyError:
+            return JsonResponse({"message": "KEY_ERROR"}, status=400)
