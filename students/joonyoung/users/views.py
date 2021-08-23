3cd0b9077fe6da773cd0f20bde980ea42d74b2f8
jayko <jayko95@naver.com>
2021-08-23 17:11:52 +0900

    Catch KeyError if -> try except User create 409 error if -> try except(use model.DoesNotExist)

--- students/joonyoung/users/views.py
+++ students/joonyoung/users/views.py
@@ -13,61 +13,45 @@ class SignUp(View):
         try:
             data = json.loads(request.body)
 
-            name         = data.get("name")
-            email        = data.get("email")
-            password     = data.get("password")
-            phone_number = data.get("phone_number")
-            bio          = data.get("bio")
+            name         = data["name"]
+            email        = data["email"]
+            password     = data["password"]
+            phone_number = data["phone_number"]
 
-            if (not email) or (not password):
+            
+            email_regex = re.compile("^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$")
+            password_regex = re.compile("^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,}$")
+
+            if not email_regex.match(email):
+                return JsonResponse({"message": "E-MAIL_VALIDATION_ERROR"}, status=400)
+
+            elif not password_regex.match(password):
+                return JsonResponse({"message": "PASSWORD_VALIDATION_ERROR"}, status=400)
+
+            try:
+                user = User.objects.get(email=email)
+                return JsonResponse({"message": "USER_ALREADY_EXIST"}, status=409)
+
+            except User.DoesNotExist:
+                user = User.objects.create(
+                    name         = name,
+                    email        = email,
+                    password     = password,
+                    phone_number = phone_number,
+                    bio          = data.get("bio"),
+                )
+
+                created_user = {
+                    "name": user.name,
+                    "email": user.email,
+                    "phone_number": user.phone_number,
+                    "bio": user.bio,
+                }
+
                 return JsonResponse(
-                    {"message": "KEY_ERROR"},
-                    status=400,
+                    {"Message": "SUCCESS", "Result": created_user}, status=201
                 )
-
-            else:
-                email_regex = re.compile(
-                    "^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
-                )
-                password_regex = re.compile(
-                    "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,}$"
-                )
-
-                if not email_regex.match(email):
-                    return JsonResponse(
-                        {"message": "E-MAIL_VALIDATION_ERROR"},
-                        status=400,
-                    )
-
-                elif not password_regex.match(password):
-                    return JsonResponse(
-                        {"message": "PASSWORD_VALIDATION_ERROR"},
-                        status=400,
-                    )
-
-                elif User.objects.filter(email=email).exists():
-                    return JsonResponse(
-                        {"message": "USER_ALREADY_EXIST"},
-                        status=409,
-                    )
-
-                else:
-                    user = User.objects.create(
-                        name         = name,
-                        email        = email,
-                        password     = password,
-                        phone_number = phone_number,
-                        bio          = bio,
-                    )
-                    created_user = {
-                        "name": user.name,
-                        "email": user.email,
-                        "phone_number": user.phone_number,
-                        "bio": user.bio,
-                    }
-
-                    return JsonResponse(
-                        {"Message": "SUCCESS", "Result": created_user}, status=201
-                    )
         except JSONDecodeError:
             return JsonResponse({"message": "JSON_DECODE_ERROR"}, status=400)
+        except KeyError:
+            return JsonResponse({"message": "KEY_ERROR"}, status=400)
