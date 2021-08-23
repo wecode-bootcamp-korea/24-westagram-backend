31e411bd9e07a17dc2c2b35c8f5451b8c693f192
jayko <jayko95@naver.com>
2021-08-23 13:29:54 +0900

    Modify User Model

--- students/joonyoung/users/models.py
+++ students/joonyoung/users/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 
 class User(models.Model):
-    name = models.CharField(max_length=30)
-    email = models.EmailField()
+    name     = models.CharField(max_length=30)
+    email    = models.EmailField()
     password = models.CharField(max_length=50)
-    contact = models.CharField(max_length=15)
-    bio = models.TextField(default="", blank=True)
+    contact  = models.CharField(max_length=15)
+    bio      = models.TextField(default="", blank=True)
 
     class Meta:
         db_table = "users"
