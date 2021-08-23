39ea528a783d323ca2ee73734bd2556dd37fa014
hyun-soo choi <hyun-soochoi@hyun-sooui-MacBookAir.local>
2021-08-23 16:38:20 +0900

    project setting views.py

--- students/hyunsu/westagram/users/views.py
+++ students/hyunsu/westagram/users/views.py
@@ -14,26 +14,22 @@ class UserView(View):
 
         try:   
             if not emali_format.search(data['email']):
-                return JsonResponse(
-                    {"message" : "Invalid Email Format"}, status=401)
+                return JsonResponse({"message" : "INVALID EMAIL FORMAT"}, status=400)
 
             if password_compare.match(data['password']):
-                return JsonResponse(
-                    {"message" : "Invalid Password Format"}, status=402)
+                return JsonResponse({"message" : "INVALID PASSWORD FORMAT"}, status=400)
 
             if User.objects.filter(email=data['email']).exists():
-                return JsonResponse(
-                    {"message":"Same Email Exist"}, status=403)
+                return JsonResponse({"message" : "SAME EMAIL EXIST"}, status=400)
             
-            user_info = User.objects.create(
+            User.objects.create(
                 name         = data['name'],
                 email        = data['email'],
                 password     = data['password'],
                 phone_number = data['phone_number'],
                 address      = data['address']
             )
+            return JsonResponse({'message' : 'SUCCESS'}, status=201)
+       
         except KeyError:
             return  JsonResponse({"message": "KEY_ERROR"}, status=400)
-
-        return JsonResponse(
-            {'message' : 'SUCCESS'}, status=201)
