c141399ba28725b46b9ade493980f988aed41cf9
김정수 <jungsookim@gimjeongsuui-MacBookAir.local>
2021-08-23 16:30:25 +0900

    Add: modification complete

--- students/jungsoo/users/models.py
+++ students/jungsoo/users/models.py
@@ -1,15 +1,13 @@
-from django.db import models
-
-# Create your models here.
-
 from phonenumber_field.modelfields import PhoneNumberField
 
+from django.db import models
+
 class User(models.Model):
-    name = models.CharField(max_length=50)
-    email = models.EmailField(max_length=50)
-    password = models.CharField(max_length=50)
-    phone_number = PhoneNumberField(unique = True, null = False, blank = False)
-    information = models.TextField()
+    name         = models.CharField(max_length=50)
+    email        = models.EmailField(max_length=50)
+    password     = models.CharField(max_length=50)
+    phone_number = PhoneNumberField(unique = True)
+    ect_info     = models.TextField()
 
     class Meta:
         db_table = 'users'
