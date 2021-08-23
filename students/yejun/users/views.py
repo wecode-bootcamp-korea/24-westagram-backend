af55b881ace5613fa40a289c5bca2a95122ed174
YeJun <hayejun1013@naver.com>
2021-08-23 18:25:01 +0900

    Fix : add white space along with code convention and delete else state

--- students/yejun/users/views.py
+++ students/yejun/users/views.py
@@ -14,25 +14,25 @@ class SignUpView(View):
         try:
             if User.objects.filter(email=data["email"]).exists():
                 return JsonResponse({"message": "EMAIL Already Exist"}, status=400)
-            else:
-                email_validator = re.compile(
-                    "^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
-                )
-                password_validator = re.compile(
-                    "^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$"
-                )
-                is_email_valid = email_validator.match(data["email"])
-                is_password_valid = password_validator.match(data["password"])
 
-                if not (is_email_valid and is_password_valid):
-                    return JsonResponse({"message": "INVALID INPUT FORMAT"}, status=400)
+            email_validator = re.compile(
+                "^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+            )
+            password_validator = re.compile(
+                "^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$"
+            )
+            is_email_valid = email_validator.match(data["email"])
+            is_password_valid = password_validator.match(data["password"])
+
+            if not (is_email_valid and is_password_valid):
+                return JsonResponse({"message": "INVALID INPUT FORMAT"}, status=400)
 
             User.objects.create(
-                name=data["name"],
-                email=data["email"],
-                password=data["password"],
-                phone_number=data["phone_number"],
-                date_of_birth=data["date_of_birth"],
+                name         = data["name"],
+                email        = data["email"],
+                password     = data["password"],
+                phone_number = data["phone_number"],
+                date_of_birth= data["date_of_birth"],
             )
 
             return JsonResponse({"message": "SUCCESS"}, status=201)
