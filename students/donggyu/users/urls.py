66cf2f5158a2dffa9c275debf84a6375dfe4da26
sdk <sdk1926@naver.com>
2021-08-23 12:57:16 +0900

    Add: LoginView

--- students/donggyu/users/urls.py
+++ students/donggyu/users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 
-from .views      import SinupView
+from .views      import SinupView, LoginView
+
 urlpatterns = [
     path('/signup', SinupView.as_view()),
+    path('/login', LoginView.as_view()),
 ]
