ca715e2e8e2c73c81c71f142a10028da3b0e4891
Melody's lap top <melody@Melodysui-MacBookPro.local>
2021-08-23 20:41:45 +0900

    Add:views_singin_V1

--- students/myeonghee/users/urls.py
+++ students/myeonghee/users/urls.py
@@ -6,5 +6,6 @@ from django.urls import path
 
 app_name="users"
 urlpatterns = [
+    path("/signup",views.SignUpView.as_view()),
     path("/signin",views.SignInView.as_view())
 ]
--- students/myeonghee/users/views.py
+++ students/myeonghee/users/views.py
@@ -7,6 +7,57 @@ from django.http import JsonResponse
 from users.models import User
 import re
 
-# Create your views here.
+class SignUpView(View):
+    
+    def post(self,request):
+        data = json.loads(request.body)
+        
+        try:
+            check_password = re.compile("^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$")
+            password_validator = check_password.match(data['password'])
 
+            if not password_validator:
+                return JsonResponse({"MESSAGE" : "PASSWORD FORM ERROR"}, status=404)
+            
+            if len(User.objects.filter(email=data["email"])) > 0:
+                return JsonResponse({"MESSAGE" : "EMAIL DUPLICATION ERROR"}, status=404)
+                
+            check_email = re.compile('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
+            email_validator = check_email.match(data["email"])
 
+            if not email_validator:
+                return JsonResponse({"MESSAGE" : "EMAIL FORM ERROR"}, status=404)
+
+            else :
+                User.objects.create(
+                name        = data["name"],
+                email       = data["email"],
+                password    = data["password"],
+                cell_number = data["cell_number"],    
+                address     = data["address"],
+                birthday    = data["birthday"],
+                sex         = data["sex"]
+            )  
+                return JsonResponse({"MESSAGE" : "CREATE"}, status=201)
+
+        except KeyError as e:
+            return JsonResponse({"MESSAGE" : "KEY_ERROR"}, status=400)
+
+class SignInView(View):
+        
+    def post (self,request):
+        data = json.loads(request.body)
+        
+        try:
+            user_check = User.objects.filter(email=data["email"])
+            
+            if not user_check.exists():
+                return JsonResponse({"MESSAGE" : "INVALID_USER"}, status=401)
+            
+            if not user_check.filter(password=data["password"]).exists:
+                return JsonResponse({"MESSAGE" : "INVALID_USER"}, status=401)
+
+            return  JsonResponse({"MESSAGE": "SUCCESS"}, status=200)
+
+        except KeyError as e:
+            return  JsonResponse({"MESSAGE": "KEY_ERROR"}, status=400)
