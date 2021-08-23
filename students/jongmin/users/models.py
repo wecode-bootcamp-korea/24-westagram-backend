3a4483b7b4866f677bf51bf20b4db7f0f37194e3
Gouache-studio <zoobellmint@naver.com>
2021-08-23 16:35:10 +0900

    [주종민] Misson 2: 모델링

--- students/jongmin/users/models.py
+++ students/jongmin/users/models.py
@@ -3,14 +3,10 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
     name         = models.CharField(max_length=45)
-    email        = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=10)
     gender       = models.CharField(max_length=45)
     address      = models.CharField(max_length=200)
     birth        = models.DateField()
-
-
-
-
-
+    email        = models.CharField(max_length=100)
+    password     = models.CharField(max_length=150)
     
--- students/jongmin/users/views.py
+++ students/jongmin/users/views.py
@@ -1,3 +1,20 @@
+import json
+
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views import View
+from users.models import User
 
 # Create your views here.
+class UsersView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        User.objects.create(
+            name         = data['name'],
+            phone_number = data['phone_number'],
+            gender       = data['gender'],
+            address      = data['address'],
+            birth        = data['birth'],
+            email        = data['email'], #정규표현신식으로
+            password     = data['password']
+        )
