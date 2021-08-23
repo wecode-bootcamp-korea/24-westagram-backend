8142c23c771694d95263313f596e4a9d77b5d63c
Young Soo Koh <kohys92@gmail.com>
2021-08-23 17:20:17 +0900

    modification applied for SIGNUP VIEW

--- students/youngsoo/users/views.py
+++ students/youngsoo/users/views.py
@@ -3,60 +3,43 @@ import re
 
 from django.views import View
 from django.http import JsonResponse
-from django.db.utils import IntegrityError
 
 from users.models import User
 
+# Registering a user 
 class SignupView(View):
-    # Registering a user 
     def post(self, request):
         try:
             data = json.loads(request.body)
 
-            email_valid = re.search('[a-zA-Z0-9.+-]+@'
+            if not re.search('[a-zA-Z0-9.+-]+@'
                                     '[a-zA-Z0-9-]+\.'
-                                    '[a-zA-Z0-9.]+', data['email'])      
-
-            password_valid = re.fullmatch('^(?=.*[a-z])(?=.*[A-Z])'
-                                          '(?=.*\d)(?=.*[@$!%*?&])'
-                                          '[A-Za-z\d@$!%*?&]{8,32}$', data['password'])
-            
-            email_duplicate = User.objects.filter(email=data['email']).exists()
-
-            if not email_valid:
+                                    '[a-zA-Z0-9.]+', data['email']) :
                 return JsonResponse({'message' : 'INVALID EMAIL'}, status = 400)
             
-            if not password_valid:
+            if not re.fullmatch('^(?=.*[a-z])(?=.*[A-Z])'
+                                          '(?=.*\d)(?=.*[@$!%*?&])'
+                                          '[A-Za-z\d@$!%*?&]{8,32}$', data['password']):
                 return JsonResponse({'message' : 'INVALID PASSWORD'}, status = 400)
 
-            if email_duplicate:
+            if User.objects.filter(email=data['email']).exists():
                 return JsonResponse({'message' : 'EMAIL DUPLICATE'}, status = 400)
             
-            else:
-                User.objects.create(
-                    name          = data['name'],
-                    email         = data['email'],
-                    password      = data['password'],
-                    phone_number  = data['phone_number'],
-                    date_of_birth = data['date_of_birth'],
-                    gender        = data['gender'],
-                    address       = data['address'],
-                )
-                return JsonResponse({'message' : 'SUCCESS'}, status = 201)
+        
+            User.objects.create(
+                name          = data['name'],
+                email         = data['email'],
+                password      = data['password'],
+                phone_number  = data['phone_number'],
+                date_of_birth = data['date_of_birth'],
+                gender        = data['gender'],
+                address       = data['address'],
+            )
+            return JsonResponse({'message' : 'SUCCESS'}, status = 201)
 
         except KeyError:
             return JsonResponse({'message' : 'KEY_ERROR'}, status = 400)
 
-
-class LoginView(View):
-    def get(self, request):
-        data = json.loads(request.body)
-        try:
-            if data['email'] in User.objects.filter(email=data['email']):
-                return JsonResponse({"message": "SUCCESS"}, status = 200)
-        
-        except KeyError:
-            return JsonResponse({"message" : "INVALID_USER"}, status = 400)
 
     
 
--- students/youngsoo/westagram/urls.py
+++ students/youngsoo/westagram/urls.py
@@ -16,5 +16,5 @@ Including another URLconf
 from django.urls import path, include
 
 urlpatterns = [
-    path('users/', include('users.urls')),
+    path('users', include('users.urls')),
 ]
