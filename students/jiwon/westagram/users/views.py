7f7bc418f823b452dab834bff24cdf189ab86507
Jiwon <jiwon@Jiwonui-MacBookAir.local>
2021-08-24 18:20:05 +0900

    project_signin

--- students/jiwon/westagram/users/views.py
+++ students/jiwon/westagram/users/views.py
@@ -6,7 +6,7 @@ from django.views    import View
 from users.models import User
 
 
-class UserView(View):
+class SignupView(View):
     def post(self, request):
         data                = json.loads(request.body)
         email_validation    = re.compile("^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$")
@@ -29,3 +29,23 @@ class UserView(View):
             favorite_food   = data['favorite_food']
             )
         return JsonResponse({'MESSAGE':'SUCCESS'}, status=201)
+
+
+class SigninView(View):
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+            # 1. email, password 받기
+            # 2. 입력된 email이 database에 존재하는지 확인
+            # 3. 입력된 email을 갖고 있는 유저의 비밀번호와, 입력된 비밀번호가 일치하는지 확인
+
+            if not User.objects.filter(email=data['email']).exists(): 
+                return JsonResponse({"message": "INVALID_USER"}, status=401)
+
+            if User.objects.get(email = data['email']).password == data['password']:
+                return JsonResponse({"message": "SUCCESS"}, status=200)
+            return JsonResponse({"message": "INVALID_USER"}, status=401)
+
+        except KeyError:
+            return JsonResponse({"message": "KEY_ERROR"}, status=400)
+            #filter(쿼리셋.exists) 와 get(객체.속성접근이 가능)
