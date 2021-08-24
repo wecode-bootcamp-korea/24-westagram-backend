b6aa00a82b1d0a25043ecccc5145e43647eae29b
Melody's lap top <melody@Melodysui-MacBookPro.local>
2021-08-24 13:01:14 +0900

    Add:views_singin_V1

--- students/myeonghee/users/views.py
+++ students/myeonghee/users/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
-
 import json
-from django.views import View
-
-from django.http import JsonResponse
-from users.models import User
 import re
+import bcrypt
+
+from django.shortcuts import render
+from django.views import View
+from django.http import JsonResponse
+
+from users.models import User
 
 class SignUpView(View):
     
