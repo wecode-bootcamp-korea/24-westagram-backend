46030393a8500ea6f0ec08c78a0f6b467fdfee8a
Jiwon <jiwon@Jiwonui-MacBookAir.local>
2021-08-24 14:50:40 +0900

    project_signup_3

--- students/jiwon/westagram/users/urls.py
+++ students/jiwon/westagram/users/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from users.views import UserView
-# 최상위 url은 앱으로 연결 / 앱안의 url은 view로 연결
 
 urlpatterns = [
-    path('', UserView.as_view())
+    path('/signup', UserView.as_view())
 ]
