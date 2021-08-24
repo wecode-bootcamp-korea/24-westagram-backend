c73a039c493d5481ed9de9b22631589b93ce0c3c
Jiwon <jiwon@Jiwonui-MacBookAir.local>
2021-08-24 18:21:18 +0900

    project_signin

--- students/jiwon/westagram/users/views.py
+++ students/jiwon/westagram/users/views.py
@@ -35,9 +35,6 @@ class SigninView(View):
     def post(self, request):
         try:
             data = json.loads(request.body)
-            # 1. email, password 받기
-            # 2. 입력된 email이 database에 존재하는지 확인
-            # 3. 입력된 email을 갖고 있는 유저의 비밀번호와, 입력된 비밀번호가 일치하는지 확인
 
             if not User.objects.filter(email=data['email']).exists(): 
                 return JsonResponse({"message": "INVALID_USER"}, status=401)
@@ -48,4 +45,3 @@ class SigninView(View):
 
         except KeyError:
             return JsonResponse({"message": "KEY_ERROR"}, status=400)
-            #filter(쿼리셋.exists) 와 get(객체.속성접근이 가능)
