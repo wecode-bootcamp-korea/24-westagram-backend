34918c35e6079a6054a37c1fb96ddfb294b6c66f
fall031 <fall900802@gmail.com>
2021-08-24 17:18:52 +0900

    ADD: project views(password).py

--- students/hyeonmuk/users/urls.py
+++ students/hyeonmuk/users/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from users.views import SigninView, UsersView
 
 urlpatterns = [
-    path('', UsersView.as_view()), 
-    path('/signin', SigninView.as_view())
+    path('/signup', UsersView.as_view()), 
+    path('/signin', SigninView.as_view()),
 ]
