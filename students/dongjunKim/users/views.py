ab7bd158b17e5edb9de9f912d6ed2bf423b21cbf
Jack Kim <hion8800@gmail.com>
2021-08-24 19:49:19 +0900

    UPDATE : token added in the server's response header

--- students/dongjunKim/users/views.py
+++ students/dongjunKim/users/views.py
@@ -1,49 +1,62 @@
 import json
 import re
+import bcrypt
+import jwt
 
 from django.http import JsonResponse
 from django.views import View
 
 from users.models import User
+from my_settings import SECRET_KEY
 
-class LoginView(View) :
-    def post(self, request) :
-        try :
+
+class LoginView(View):
+    def post(self, request):
+        try:
             data = json.loads(request.body)
 
-            if !User.objects.filter(email=data['email']).exists() :
+            if not User.objects.filter(email=data['email']).exists():
                 return JsonResponse({'MESSAGE':'INVALID_USER'}, status=401)
 
             user = User.objects.get(email=data['email'])
 
-            if user.password != data['password'] :
+            if not bcrypt.checkpw(data['password'].encode('utf-8'), user.password.encode('utf-8')):
                 return JsonResponse({'MESSAGE':'INVALID_USER'}, status=401)
 
+            encoded_jwt = jwt.encode({'user-id':user.id}, SECRET_KEY, algorithm='HS256')
+            response = JsonResponse({'MESSAGE':'SUCCESS'}, status=200)
+            response['access-token'] = encoded_jwt
+            
             return JsonResponse({'MESSAGE':'SUCCESS'}, status=200)
+        
+        except KeyError:
+            return JsonResponse({'MESSAGE':'KEY_ERROR'}, status=400)
 
-class SignupView(View) :
-    def post(self, request) :
-        try :
+class SignupView(View):
+    def post(self, request):
+        try:
             data = json.loads(request.body)
 
-            if not re.compile('^[a-zA-Z0-9!#$%^&*()]+@[a-z]+.[a-z]+$').match(data['email']) :
+            if not re.compile('^[a-zA-Z0-9!#$%^&*()]+@[a-z]+.[a-z]+$').match(data['email']):
                 return JsonResponse({'MESSAGE':'INVALID_EMAIL_ERROR'},status=400)
 
-            if not re.compile('^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,100}$').match(data['password']) :
+            if not re.compile('^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,100}$').match(data['password']):
                 return JsonResponse({'MESSAGE':'INVALID_PASSWORD_ERROR'},status=400)
 
-            if User.objects.filter(email=email).exists() :
+            if User.objects.filter(email=data['email']).exists():
                 return JsonResponse({'MESSAGE':'DUPLICATION_ERROR'}, status=400)
+
+            hashed_password = bcrypt.hashpw( data['password'].encode('utf-8'), bcrypt.gensalt() )
 
             User.objects.create(
                 name     = data['name'],
                 email    = data['email'],
-                password = data['password'],
+                password = hashed_password.decode(),
                 phone    = data['phone']
             )
-
             return JsonResponse({'MESSAGE':'SUCCESS'}, status=201)
 
-        except KeyError :
+        except KeyError:
             return JsonResponse({'MESSAGE':'KEY_ERROR'}, status=400)
+
 
