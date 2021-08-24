8bafcbe7a674ffa5eb76bc4fd12da16bfba1acbf
신우주 <newspace@sin-ujuui-MacBookPro.local>
2021-08-24 11:25:24 +0900

    project login

--- students/wooju/users/urls.py
+++ students/wooju/users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from users.views import UsersView
+from users.views import LoginsView, UsersView
 
 urlpatterns = [
     path('/sign-up', UsersView.as_view()),
+    path('/login', LoginsView.as_view()),
 ]  
