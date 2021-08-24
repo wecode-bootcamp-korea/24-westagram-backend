ba392e41d76ddbfb3696db43427ab23e244808de
Melody's lap top <melody@Melodysui-MacBookPro.local>
2021-08-24 15:54:17 +0900

    modify:sign&singup_V1

--- students/myeonghee/users/models.py
+++ students/myeonghee/users/models.py
@@ -4,7 +4,7 @@ class User(models.Model):
 
     name        = models.CharField(max_length=45)
     email       = models.EmailField()
-    password    = models.CharField(max_length=16)
+    password    = models.CharField(max_length=500)
     cell_number = models.CharField(max_length=13)
     address     = models.CharField(max_length=50)
     birthday	= models.DateField()
--- students/myeonghee/users/views.py
+++ students/myeonghee/users/views.py
@@ -1,12 +1,71 @@
-from django.shortcuts import render
-
 import json
-from django.views import View
-
-from django.http import JsonResponse
-from users.models import User
+import bcrypt
 import re
 
-# Create your views here.
+from django.views import View
+from django.http import JsonResponse
 
+from users.models import User
 
+class SignUpView(View):
+    
+    def post(self,request):
+        data = json.loads(request.body)
+        
+        try:
+            check_password     = re.compile("^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$")
+            password_validator = check_password.match(data['password'])
+
+            if not password_validator:
+                return JsonResponse({"MESSAGE" : "PASSWORD FORM ERROR"}, status=404)
+            
+            if len(User.objects.filter(email=data["email"])) > 0:
+                return JsonResponse({"MESSAGE" : "EMAIL DUPLICATION ERROR"}, status=404)
+                
+            check_email     = re.compile('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
+            email_validator = check_email.match(data["email"])
+
+            if not email_validator:
+                return JsonResponse({"MESSAGE" : "EMAIL FORM ERROR"}, status=404)
+
+            # origin_password  = data['password']
+            # hashed_password  = bcrypt.hashpw(origin_password.encode('utf-8'), bcrypt.gensalt())
+            # decoded_hash_pwd = hashed_password.decode('utf-8')  
+
+            User.objects.create(
+            name        = data["name"],
+            email       = data["email"],
+            # password    = decoded_hash_pwd,
+            password    = data['password'],
+            cell_number = data["cell_number"],    
+            address     = data["address"],
+            birthday    = data["birthday"],
+            sex         = data["sex"]
+            )  
+            return JsonResponse({"MESSAGE" : "CREATE"}, status=201)
+
+        except KeyError as e:
+            return JsonResponse({"MESSAGE" : "KEY_ERROR"}, status=400)
+
+class SignInView(View):
+        
+    def post (self,request):
+        data = json.loads(request.body)
+        
+        try:
+            if not User.objects.filter(email=data["email"]).exists():
+                return JsonResponse({"MESSAGE" : "INVALID_USER"}, status=401)
+            
+            entered_pwd       = data['password']
+            encoded_enter_pwd = entered_pwd.encode('utf-8')
+
+            user_pwd         = User.objects.get(email=data["email"]).password
+            encoded_user_pwd = user_pwd.encode('utf-8')
+
+            if bcrypt.checkpw(encoded_enter_pwd,encoded_user_pwd):
+                return  JsonResponse({"MESSAGE": "SUCCESS"}, status=200)
+            else:    
+                return  JsonResponse({"MESSAGE": "KEY_ERROR"}, status=200)
+
+        except KeyError as e:
+            return  JsonResponse({"MESSAGE": "KEY_ERROR"}, status=400)
