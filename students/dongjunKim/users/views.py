d3c91831ded3bb064d9647d77d5f9117605265e6
Jack Kim <hion8800@gmail.com>
2021-08-24 17:25:35 +0900

    UPDATE : SIGNUP ENCRYPT

--- students/dongjunKim/users/views.py
+++ students/dongjunKim/users/views.py
@@ -7,6 +7,21 @@ from django.views import View
 
 from users.models import User
 
+class LoginView(View) :
+    def post(self, request) :
+        try :
+            data = json.loads(request.body)
+
+            if !User.objects.filter(email=data['email']).exists() :
+                return JsonResponse({'MESSAGE':'INVALID_USER'}, status=401)
+
+            user = User.objects.get(email=data['email'])
+
+            if user.password != data['password'] :
+                return JsonResponse({'MESSAGE':'INVALID_USER'}, status=401)
+
+            return JsonResponse({'MESSAGE':'SUCCESS'}, status=200)
+
 class SignupView(View) :
     def post(self, request) :
         try :
@@ -18,20 +33,18 @@ class SignupView(View) :
             if not re.compile('^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,100}$').match(data['password']) :
                 return JsonResponse({'MESSAGE':'INVALID_PASSWORD_ERROR'},status=400)
 
-            if not User.objects.filter(email=email).exists() :
+            if User.objects.filter(email=data['email']).exists() :
                 return JsonResponse({'MESSAGE':'DUPLICATION_ERROR'}, status=400)
 
-	    hashed_password = bcrypt.hashpw(data['password'].encode('utf-8'), bycrypt.gensalt())
+            hashed_password = bcrypt.hashpw( data['password'].encode('utf-8'), bcrypt.gensalt() )
 
-	    User.objects.create(
-		name     = data['name'],
-		email    = data['email'],
-		password = hashed_password,
-		phone    = data['phone']
-	    )
-
+            User.objects.create(
+                name     = data['name'],
+                email    = data['email'],
+                password = hashed_password,
+                phone    = data['phone']
+            )
             return JsonResponse({'MESSAGE':'SUCCESS'}, status=201)
 
         except KeyError :
             return JsonResponse({'MESSAGE':'KEY_ERROR'}, status=400)
-
