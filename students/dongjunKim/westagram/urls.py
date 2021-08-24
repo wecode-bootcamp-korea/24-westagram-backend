1b728143415100bf75d5a32885ba91f78e59f3e5
Jack Kim <hion8800@gmail.com>
2021-08-24 16:33:57 +0900

    ADD : singup encryption

--- students/dongjunKim/westagram/urls.py
+++ students/dongjunKim/westagram/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
-from users.views import SignupView
+from django.urls import include, path
+from users.views import SignupView, LoginView
 
+user_urls = [
+    path('/signup', SignupView.as_view()),
+    path('/login', LoginView.as_view())
+]
 urlpatterns = [
-    path('signup', SignupView.as_view()),
+    path('users', include(user_urls)),
 ]
