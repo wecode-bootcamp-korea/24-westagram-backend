30b4684960c38cc9ac08e8cc6975dd4c6269f652
Melody's lap top <melody@Melodysui-MacBookPro.local>
2021-08-24 13:08:20 +0900

    Add:views_singin_V2

--- students/myeonghee/users/views.py
+++ students/myeonghee/users/views.py
@@ -12,7 +12,7 @@ class SignUpView(View):
         data = json.loads(request.body)
         
         try:
-            check_password = re.compile("^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$")
+            check_password     = re.compile("^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$")
             password_validator = check_password.match(data['password'])
 
             if not password_validator:
@@ -21,23 +21,22 @@ class SignUpView(View):
             if len(User.objects.filter(email=data["email"])) > 0:
                 return JsonResponse({"MESSAGE" : "EMAIL DUPLICATION ERROR"}, status=404)
                 
-            check_email = re.compile('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
+            check_email     = re.compile('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
             email_validator = check_email.match(data["email"])
 
             if not email_validator:
                 return JsonResponse({"MESSAGE" : "EMAIL FORM ERROR"}, status=404)
 
-            else :
-                User.objects.create(
-                name        = data["name"],
-                email       = data["email"],
-                password    = data["password"],
-                cell_number = data["cell_number"],    
-                address     = data["address"],
-                birthday    = data["birthday"],
-                sex         = data["sex"]
+            User.objects.create(
+            name        = data["name"],
+            email       = data["email"],
+            password    = data["password"],
+            cell_number = data["cell_number"],    
+            address     = data["address"],
+            birthday    = data["birthday"],
+            sex         = data["sex"]
             )  
-                return JsonResponse({"MESSAGE" : "CREATE"}, status=201)
+            return JsonResponse({"MESSAGE" : "CREATE"}, status=201)
 
         except KeyError as e:
             return JsonResponse({"MESSAGE" : "KEY_ERROR"}, status=400)
