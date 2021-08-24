da96daf71f3b46e7ad9f2fb94d1467663b94cb05
Jack Kim <hion8800@gmail.com>
2021-08-24 12:42:36 +0900

    UPDATE : resolving..

--- students/dongjunKim/users/views.py
+++ students/dongjunKim/users/views.py
@@ -6,21 +6,17 @@ from django.views import View
 
 from users.models import User
 
-
 class LoginView(View) :
-    def post(self, request) :        
+    def post(self, request) :
         try :
             data = json.loads(request.body)
-       
+
             if !User.objects.filter(email=data['email']).exists() :
                 return JsonResponse({'MESSAGE':'INVALID_USER'}, status=401)
 
             user = User.objects.get(email=data['email'])
-        
+
             if user.password != data['password'] :
                 return JsonResponse({'MESSAGE':'INVALID_USER'}, status=401)
-            
-            return JsonResponse({'MESSAGE':'SUCCESS'}, status=200)
 
-        except KeyError :
-            return JsonResponse({'MESSAGE':'KEY_ERROR'}, status=400)
+            return JsonResponse({'MESSAGE':'SUCCESS'}, status=200)
