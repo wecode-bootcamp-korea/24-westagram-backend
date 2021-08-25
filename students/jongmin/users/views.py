64ecd0871fb9aa8f895093953ceb5b7ce1d90f5c
Gouache-studio <zoobellmint@naver.com>
2021-08-25 11:46:56 +0900

    [주종민] Misson 5: 회원가입 비밀번호 암호화 적용

--- students/jongmin/users/views.py
+++ students/jongmin/users/views.py
@@ -1,11 +1,13 @@
 import json
 import re 
 import bcrypt
+#import jwt
 
 from django.http import JsonResponse
 from django.views import View
 
 from users.models import User
+from my_settings import SECRET_KEY
 
 # Create your views here.
 class Signup(View):
@@ -40,8 +42,7 @@ class Signup(View):
                 address       = data['address'],
                 birth         = data['birth'],
                 email         = data['email'],
-                password      = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-
+                password      = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
             )
             return JsonResponse({"MESSAGE":"SUCCESS"}, status=201) 
           
@@ -60,14 +61,17 @@ class Login(View):
                 return JsonResponse({"MESSAGE": "INVALID_USER"}, status=401)
             
             user = User.objects.get(email = email)
-            if user.password != password:
-                return JsonResponse({"MESSAGE": "INVALID_PASSWORD"}, status=401)
+            # if not bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
+            #     return JsonResponse({"MESSAGE": "INVALID_PASSWORD"}, status=401)
 
-            return JsonResponse({"MESSAGE":"SUCCESS"}, status=200)
+            # ticket = jwt.encode({'id':user.id}, SECRET_KEY, algorithm='HS256') 
+            return JsonResponse({"MESSAGE":"SUCCESS"}, {"TOKEN": ticket}, status=200)
+               
+
 
         except ValueError:
             return JsonResponse({"MESSAGE":"VALUE_ERROR"}, status=400)
-        
+
         except KeyError:
             return JsonResponse({"MESSAGE":"KEY_ERROR"}, status=400)
 
