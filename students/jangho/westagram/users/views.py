6e74670e9e58799bf5e71d9ef40fb365ee099c20
Hope Kim <kzang2028@gmail.com>
2021-08-28 20:56:37 +0900

    Add: added password encryption by bcrypt & jwt issuer

--- students/jangho/westagram/users/views.py
+++ students/jangho/westagram/users/views.py
@@ -1,4 +1,5 @@
-import json
+from datetime import datetime, timedelta
+import json, bcrypt, jwt
 
 from django.http import JsonResponse, HttpResponse
 from django.views import View
@@ -6,6 +7,8 @@ from django.db.utils import DataError
 
 from users.models import  User
 from . validation import email_validation, nickname_validation, password_validation
+from my_settings import SECRET_KEY
+
 
 class SignUpView(View):
     def post(self, request):
@@ -23,11 +26,17 @@ class SignUpView(View):
                 return JsonResponse({'message': 'REGISTERED_USER'}, status=400)
             if User.objects.filter(nickname=data['nickname']).exists():
                 return JsonResponse({'message': 'REGISTERED_NICKNAME'}, status=400)
+
+            salt = bcrypt.gensalt()
+            encoded_password = data['password'].encode('utf-8')
+            hashed_password = bcrypt.hashpw(encoded_password, salt)
+            decoded_password = hashed_password.decode('utf-8')
+
             User.objects.create(
                 name     = data['name'],
                 nickname = data['nickname'],
                 email    = data['email'], 
-                password = data['password'],
+                password = decoded_password,
                 phone    = data['phone']
             )
             return JsonResponse({'message': 'SUCCESS!'}, status=201)
@@ -41,14 +50,35 @@ class SignUpView(View):
 class SignInView(View):
     def post(self, request):
         data = json.loads(request.body)
+        user = User.objects.get(email=data['email'])
 
         try:
             if User.objects.filter(email=data['email']).exists():
-                if User.objects.get(email=data['email']).password != data['password']:
-                    return JsonResponse({'message': 'INVALID_PASSWORD'}, status=401)
-                return JsonResponse({'message': 'LOGIN_SUCCESS'}, status=200)
+                encoded_input_password   = data['password'].encode('utf-8')
+                encoded_db_password      = (user.password).encode('utf-8')
+                access_token_expire_date = datetime.utcnow() + timedelta(hours=3)
+    
+                if bcrypt.checkpw(encoded_input_password, encoded_db_password):
+                    access_token = jwt.encode(
+                        {
+                            'email': user.email,
+                            'exp'  : access_token_expire_date
+                        }, 
+                        SECRET_KEY, algorithm = 'HS256'
+                    )
+                    return JsonResponse(
+                        {
+                            'message'    : 'LOGIN_SUCCESS', 
+                            'token'      : access_token, 
+                            'expire date': access_token_expire_date.strftime('%Y/%m/%d %H:%M:%S')
+                        }, status=200
+                    )
+
+                return JsonResponse({'message': 'INVALID_PASSWORD'}, status=401)
             return JsonResponse({'message': 'INVALID_USER'}, status=401)
         
         except KeyError:
             return JsonResponse({'message': 'KEY_ERROR'}, status=400)
+        except jwt.ExpiredSignatureError:
+            return JsonResponse({'message': 'EXPIRED_TOKEN'}, status=401)
         
